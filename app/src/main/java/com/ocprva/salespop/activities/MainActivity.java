93597b8e054ac3064e886e3c66255c8b3484e0a6
octapri03 <octapri03@gmail.com>
2023-01-31 10:22:54 +0100

    style

--- app/src/main/java/com/ocprva/salespop/activities/MainActivity.java
+++ app/src/main/java/com/ocprva/salespop/activities/MainActivity.java
@@ -1,9 +1,12 @@
 package com.ocprva.salespop.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ocprva.salespop.R;
 
 
@@ -13,5 +16,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.homeMenu:
+
+                        break;
+
+                    case R.id.categoriesMenu:
+                        // Handle icon 2 click
+                        break;
+
+                    case R.id.sellMenu:
+                        // Handle
+                        break;
+
+                    case R.id.favsMenu:
+                    // Handle
+                    break;
+
+                    case R.id.userMenu:
+                        // Handle
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+}
 }
